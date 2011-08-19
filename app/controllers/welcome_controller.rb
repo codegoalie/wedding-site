2c6a2f44051140f670bd04629f03078b966cb6ba
Chris Marshall <chris@chrismar035.com>
2011-08-19 11:30:32 -0400

    Added image gallery, but not working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,8 @@ class WelcomeController < ApplicationController
 
   def pics
     @title = "Picture Gallery"
+
+    @images = Dir.entries("#{Rails.public_path}/images/image_gallery").delete_if{ |f| f =~ /^\.+$/ }
   end
 
 end
