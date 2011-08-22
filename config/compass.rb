f4b668f508cc38629b5a348fc5877e1e93508e85
Chris Marshall <cmarshall@wallstreetsystemsinc.com>
2011-08-22 13:28:28 -0400

    Fixed fonts; properly initialized compass

--- config/compass.rb
+++ config/compass.rb
@@ -4,6 +4,9 @@ project_type = :rails
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+css_dir = "public/stylesheets"
+sass_dir = "app/stylesheets"
+images_dir = "public/images"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
