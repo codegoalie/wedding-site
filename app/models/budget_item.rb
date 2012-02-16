f9cf680956797da1bdc7ac32c78667ea2d486d29
Chris Marshall <chris@chrismar035.com>
2012-02-15 21:51:06 -0500

    Better BudgetItem validations

--- app/models/budget_item.rb
+++ app/models/budget_item.rb
@@ -3,6 +3,8 @@ class BudgetItem < ActiveRecord::Base
   attr_accessible :type, :units, :rate, :title, :balance_due_on
 
   validates_presence_of :rate, :title, :type
+  validates_numericality_of :rate
+  validates_presence_of :units, :if => "type == 'BudgetItem'"
 
   def total
     units * rate
