51eb22d4a3febfbbc7d1450e72febed3e9065243
Chris Marshall <chris@chrismar035.com>
2012-05-16 23:03:02 -0400

    Fix full_friendly_name for Guest

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -5,7 +5,9 @@ class Guest < ActiveRecord::Base
   attr_accessible :name, :email, :address, :city, :state, :zip, :count, :and_guest, :friendly_name
 
   def full_friendly_name
-    full_name = @friendly_name || @name
-    full_name << " and Guest" if @and_guest
+    full_name = self.friendly_name || self.name
+    full_name << " and Guest" if self.and_guest
+
+    full_name
   end
 end
