85f04d6021a780a845f4cd42c91947447b651c55
Chris Marshall <chris@chrismar035.com>
2012-05-20 13:39:35 -0400

    Must have from email address; no truncate meal titles

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -6,7 +6,8 @@ class RsvpMailer < ActionMailer::Base
     @not_attending = Invitation.where(:attending => false).where('invitations.created_at <> invitations.updated_at').joins(:guest).sum('guests.count').to_i
     @percent_reporting = (1.0 * (@attending.to_i + @not_attending.to_i) / @total_invited.to_i).round(2) * 100
 
-    mail(:to => User.all.map(&:email).join(','), 
+    mail(:to => User.all.map(&:email).join(','),
+         :from => 'rsvp@crayley.com',
          :subject => "#{@invitation.guest.full_friendly_name} has RSVPed: #{@invitation.attending ? 'YES' : 'NO'}")
 
   end
