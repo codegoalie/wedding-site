f91dda6433c6560b730c048e18e2f975125d34f7
Chris Marshall <chris@chrismar035.com>
2012-07-01 17:17:26 -0400

    Round attending % to two decimal places

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -7,7 +7,7 @@ class AttendeesController < ApplicationController
     @total_invited = Invitation.joins(:guest).sum('guests.count')
     @attending = @attendees.size
     @not_attending = Invitation.where(:attending => false).where('invitations.created_at <> invitations.updated_at').joins(:guest).sum('guests.count').to_i
-    @percent_reporting = (1.0 * (@attending.to_i + @not_attending.to_i) / @total_invited.to_i).round(2) * 100
+    @percent_reporting = (1.0 * (@attending.to_i + @not_attending.to_i) / @total_invited.to_i).round(4) * 100
 
     respond_to do |format|
       format.html # index.html.erb
--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -4,7 +4,7 @@ class RsvpMailer < ActionMailer::Base
     @total_invited = Invitation.joins(:guest).sum('guests.count')
     @attending = Attendee.count
     @not_attending = Invitation.where(:attending => false).where('invitations.created_at <> invitations.updated_at').joins(:guest).sum('guests.count').to_i
-    @percent_reporting = (1.0 * (@attending.to_i + @not_attending.to_i) / @total_invited.to_i).round(2) * 100
+    @percent_reporting = (1.0 * (@attending.to_i + @not_attending.to_i) / @total_invited.to_i).round(4) * 100
 
     mail(:to => User.all.map(&:email).join(','),
          :from => 'rsvp@crayley.com',
